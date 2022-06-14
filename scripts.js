bb773cde0bde511eed6b0958ce094c75cc5dc721
giancarlocorrea <95420878+giancarlocorrea@users.noreply.github.com>
2022-06-14 14:40:47 -0300

    better logic

--- scripts.js
+++ scripts.js
@@ -1,20 +1,21 @@
 function calculateTip(event) {
   event.preventDefault()
   let bill = document.getElementById('bill').value
-  let serviceQual = document.getElementById('serviceQual').value
+  let serviceQual = Number(document.getElementById('serviceQual').value)
   let numOfPeople = document.getElementById('people').value
+  let total = 0
 
-  if ((bill == '') | (serviceQual == 0)) {
+  if (bill == '' || serviceQual == 0) {
     alert('Por favor, preencha os valores!')
     return
   }
 
-  if ((numOfPeople == '') | (numOfPeople <= 1)) {
+  if (numOfPeople == '' || numOfPeople <= 1) {
     numOfPeople = 1
     document.getElementById('each').style.display = 'none'
   } else document.getElementById('each').style.display = 'block'
 
-  let total = (bill * serviceQual) / numOfPeople
+  total = (bill * serviceQual) / numOfPeople
 
   document.getElementById('tip').innerHTML = `R$${total.toFixed(2)}`
   document.getElementById('totalTip').style.display = 'flex'
